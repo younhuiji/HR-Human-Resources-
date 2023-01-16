4d815c61272d44a85247168dd1c291630212f2ef
juhwan <asdcz@naver.com>
2023-01-16 20:47:13 +0900

    Member table

---
+++ src/main/java/com/sohwakmo/hr/domain/EmployeePosition.java
@@ -0,0 +1,8 @@
+package com.sohwakmo.hr.domain;
+
+public enum EmployeePosition {
+    LEVEL_1, // 일반 사원
+    LEVEL_2, // 대리, 차장, 과장
+    LEVEL_3, // 팀장 이상
+    LEVEL_4 // Admin
+}
---
+++ src/main/java/com/sohwakmo/hr/domain/Part.java
@@ -0,0 +1,20 @@
+package com.sohwakmo.hr.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Part {
+
+    @Column(name = "DEPARTMENT_NAME",nullable = false)
+    private String department; // 부서명
+
+    @Column(name = "TEAM_NAME",nullable = false)
+    private String team;
+
+    @Column(name = "ASSIGNED_WORK",nullable = false)
+    private String work;
+
+    @Column(nullable = false)
+    private String position; // 직책
+}
