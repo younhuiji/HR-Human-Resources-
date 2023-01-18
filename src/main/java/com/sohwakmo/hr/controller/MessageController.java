f447f5a6dbb2f189766f3cf20f15a6e77af52403
swim <sijkok@naver.com>
2023-01-18 14:22:39 +0900

    레이아웃 오류 수정

---
+++ src/main/java/com/sohwakmo/hr/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.sohwakmo.hr.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/message")
+public class MessageController {
+
+    @GetMapping("/receiveList")
+    public String receiveList() {
+        log.info("receiveList()");
+
+
+
+        return "/message/receiveList";
+    }
+
+}
