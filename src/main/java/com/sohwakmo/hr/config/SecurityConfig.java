e0c23995c7c0a4080412930618e277353d6d2530
juhwan <asdcz@naver.com>
2023-01-23 23:50:04 +0900

    스프링 시큐리티 config 설정중

--- src/main/java/com/sohwakmo/hr/config/SecurityConfig.java
+++ src/main/java/com/sohwakmo/hr/config/SecurityConfig.java
@@ -1,9 +1,34 @@
 package com.sohwakmo.hr.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity //
 public class SecurityConfig {
+
+    @Bean // 스프링 컨텍스트에서 생성, 관리하는 객체 - 필요한 곳에 의존성 주입.
+    // 암호화(복호화) 알고리즘 객체
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
 }
