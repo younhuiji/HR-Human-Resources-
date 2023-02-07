bb0cf32dbb0a3caecf891705b96ff16f0aab6fdb
ByeonJuHwan <asdcz@naver.com>
2023-02-07 13:39:23 +0900

    비밀번호찾기　디자인중

--- src/main/resources/static/js/employee/resetPw.js
+++ src/main/resources/static/js/employee/resetPw.js
@@ -6,4 +6,21 @@ window.addEventListener('DOMContentLoaded',function (){
         form.method = "post";
         form.submit();
     })
+
+
+    // 비밀번호 가져오기
+    const password_input = document.querySelector('#password_input');
+    const passwordNotOkMsg = document.querySelector('#passwordNotOkMsg');
+    password_input.addEventListener('keydown', function (){
+        let pw_length = password_input.value.length
+        if (pw_length< 7) {
+            passwordNotOkMsg.className = '';
+            password_input.classList.add('border_danger','error_icon')
+            password_input.classList.remove('border_success','ok_icon');
+        }else{
+            passwordNotOkMsg.className = 'd-none';
+            password_input.classList.add('border_success','ok_icon');
+            password_input.classList.remove('border_danger','error_icon');
+        }
+    })
 })
