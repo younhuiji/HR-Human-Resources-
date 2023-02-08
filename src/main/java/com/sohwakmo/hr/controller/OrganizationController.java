741ddb2b8d0ce99c3c853f5fd1795c50b381597d
soljeon23 <soljeon23@gmail.com>
2023-02-08 13:55:49 +0900

    일정:로그인사용자 변경함

--- src/main/java/com/sohwakmo/hr/controller/OrganizationController.java
+++ src/main/java/com/sohwakmo/hr/controller/OrganizationController.java
@@ -2,6 +2,7 @@ package com.sohwakmo.hr.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,5 +17,9 @@ public class OrganizationController {
     }
 
     @GetMapping("/calendar")
-    public void calendar(){log.info("calendar()");}
+    public void calendar(String loginUser, Model model){
+        log.info("calendar(loginUser={})",loginUser);
+
+        model.addAttribute("loginUser",loginUser);
+    }
 }
