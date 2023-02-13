09f3b805f9b8c4ccc569a5da07ac03992266a717
swim <sijkok@naver.com>
2023-02-13 14:02:25 +0900

    쪽지쓰기 내용 미 입력 시 진행 못하도록 막음

--- src/main/resources/static/js/message/writeMessage.js
+++ src/main/resources/static/js/message/writeMessage.js
@@ -12,9 +12,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const formBody  = document.querySelector("#formBody");
     const btnSend   = document.querySelector("#btnSend");
+    const title = document.querySelector("#title");
+    const receiveName = document.querySelector("#receiveName");
+    const content = document.querySelector("#content");
+
     // 쪽지쓰기 버튼
     btnSend.addEventListener("click", function(e) {
         e.preventDefault();
+
+        if(title.value == '') { alert("제목을 입력해 주세요."); return;}
+        if(receiveName.value == '') { alert("수신자를 선택해 주세요."); return; }
+        if(content.value == '') { alert("내용을 입력해 주세요."); return; }
+
         const result = confirm("보내시겠습니까?")
         if(result) {
             formBody.action = '/message/sendMessage';
