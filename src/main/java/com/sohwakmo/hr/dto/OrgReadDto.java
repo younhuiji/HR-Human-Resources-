4eba3e162fc5958d42035d0c973efd82991c113b
MinYss <saocwxx116@naver.com>
2023-02-14 09:30:40 +0900

    수정4-참석자사번

--- src/main/java/com/sohwakmo/hr/dto/OrgReadDto.java
+++ src/main/java/com/sohwakmo/hr/dto/OrgReadDto.java
@@ -15,6 +15,7 @@ import lombok.ToString;
 public class OrgReadDto {
 
     private Long id;
+    private String employeeNo;
     private String name;
     private String phone;
     private String photo;
@@ -28,6 +29,7 @@ public class OrgReadDto {
     public static OrgReadDto fromEntity(Employee entity){
         return OrgReadDto.builder()
                 .id(entity.getId())
+                .employeeNo(entity.getEmployeeNo())
                 .name(entity.getName())
                 .phone(entity.getPhone())
                 .photo(entity.getPhoto())
